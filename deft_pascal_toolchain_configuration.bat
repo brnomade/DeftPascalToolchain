949d0bf80e6c9719f004a6d272d77b8d3c01a00e
Andre Ballista <36184011+brnomade@users.noreply.github.com>
2018-04-19 16:23:56 +0100

    Add files via upload
    Configuration file for the toolchain containing folder and application names.

---
+++ deft_pascal_toolchain_configuration.bat
@@ -0,0 +1,7 @@
+SET EDITOR_FOLDER=E:\Program Files (x86)\ConTEXT
+SET EDITOR_EXECUTABLE=ConTEXT.exe
+SET DEFT_DSK_FOLDER=C:\Users\Andre\Downloads\TRS COLOR\DeftPascal\Test
+SET DEFT_DSK_FILE=deft.dsk
+SET DEFT_PROJECTS_FOLDER=C:\Users\Andre\Downloads\TRS COLOR\DeftPascal\Test\Projects
+SET MAME_FOLDER=C:\Users\Andre\Downloads\TRS COLOR\mame0196b_64bit
+SET AUTOIT_FOLDER=E:\Program Files (x86)\AutoIt3
