cb5438159a59f106d358d9892f4a7c0c0f0790f6
brnomade <36184011+brnomade@users.noreply.github.com>
2021-11-21 20:10:53 +0000

    compilation errors tracking

--- dptc/dptc.py
+++ dptc/dptc.py
@@ -543,6 +543,7 @@ class DeftPascalToolChain:
         oscommand = oscommand + " -autoboot_script " + os.path.join(self._args.project_folder, boot_script_file)
         try:
             result = subprocess.run(oscommand, capture_output=True, text=True)
+            print(result.stdout)
         except:
             print("\nAborting execution. Unable to start emulator.")
             print("Check if '{0}' is located in '{1}'.".format(self._args.emulator_app, self._args.compiler_folder))
